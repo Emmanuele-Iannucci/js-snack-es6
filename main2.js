f01d0e20e1a2af9d0fc50d4ef187707f76732fe3
Emmanuele-Iannucci <emmanueleiannucci@gmail.com>
2025-07-26 15:27:50 +0200

    add_teams_array

--- main2.js
+++ main2.js
@@ -8,5 +8,41 @@ Creo un nuovo array con solo nome e falli subiti.
 
 Stampo tutto in console. */
 
+// Creo un array di oggetti che rappresentano squadre di calcio
+// Ogni squadra ha un nome, punti fatti e falli subiti (inizializzati a 0)
+
+const teams = [
+  {
+    id: 1,
+    nome: 'Juventus',
+    punti: 0,
+    falli: 0
+  },
+  {
+    id: 2,
+    nome: 'Inter',
+    punti: 0,
+    falli: 0
+  },
+  {
+    id: 3,
+    nome: 'Milan',
+    punti: 0,
+    falli: 0
+  },
+  {
+    id: 4,
+    nome: 'Roma',
+    punti: 0,
+    falli: 0
+  },
+  {
+    id: 5,
+    nome: 'Napoli',
+    punti: 0,
+    falli: 0
+  }
+];
+
 
 
