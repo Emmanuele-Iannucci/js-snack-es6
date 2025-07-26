eadbbb21d9e9575698629f825d53affab81ccfb0
Emmanuele-Iannucci <emmanueleiannucci@gmail.com>
2025-07-26 14:30:23 +0200

    add_array_objects

--- main.js
+++ main.js
@@ -1,3 +1,30 @@
 // Creiamo un array di oggetti, ognuno rappresenta una bici da corsa
 // Ogni oggetto ha un nome e un peso
 
+const RoadBike = [
+  {
+    id: 1,
+    nome: 'Bianchi',
+    peso: 7
+  },
+  {
+    id: 2,
+    nome: 'Pinarello',
+    peso: 6.5
+  },
+  {
+    id: 3,
+    nome: 'Colnago',
+    peso: 7.2
+  },
+  {
+    id: 4,
+    nome: 'Cannondale',
+    peso: 6.8
+  },
+  {
+    id: 5,
+    nome: 'Trek',
+    peso: 6.3
+  }
+];
