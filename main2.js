f4a6fd2c3f8000ef43a32ddba93879b74336857e
Emmanuele-Iannucci <emmanueleiannucci@gmail.com>
2025-07-26 15:24:36 +0200

    add_comments

---
+++ main2.js
@@ -0,0 +1,12 @@
+/* Creo un array di oggetti che rappresentano squadre di calcio.
+
+Ogni squadra ha:
+nome (inserito manualmente)
+puntiFatti e falliSubiti che inizializzo a 0
+Genero valori randomici per punti e falli.
+Creo un nuovo array con solo nome e falli subiti.
+
+Stampo tutto in console. */
+
+
+
